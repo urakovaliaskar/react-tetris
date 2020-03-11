e706d1eaafe83482e5ce1c73d9a64bdcfc48e115
urakovaliaskar <urakov.aliaskar@gmail.com>
2020-03-11 10:13:43 +0600

    initial commit

---
+++ src/components/Stage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import {StyledStage} from './styles/StyledStage';
+
+import Cell from './Cell';
+
+const Stage = ({stage}) => (
+    <StyledStage width={stage[0].length} height={stage.length}>
+        {stage.map(row => row.map((cell, i) => <Cell key={i} type={cell[0]} />))}
+    </StyledStage>
+)
+
+export default Stage;
